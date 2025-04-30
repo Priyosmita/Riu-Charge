200bd7effe59ca9655ad9352f25cc9c31a6b019d
Priyosmita <daspriyosmita2003@gmail.com>
2025-05-01 01:27:16 +0530

    header in progress

---
+++ app/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { GrDown } from "react-icons/gr";
+
+const Header = () => {
+    return (
+        <>
+            <div className='flex flex-row justify-center gap-x-52 h-20 items-center bg-[#EBF2FB]'>
+                <div className='bg-[#F16122] cursor-pointer'>Riu Charge</div>
+
+                <div className='flex flex-row text-black gap-14 text-md'>
+                    <span className='hover:text-[#F16122] transform duration-300 cursor-pointer'>Home</span>
+                    
+                    <span className='flex flex-row gap-2 hover:text-[#F16122] transform duration-300 cursor-pointer'>Consumer Payments <GrDown className='pt-1 text-xl'/></span>
+                    
+                    <span className='hover:text-[#F16122] transform duration-300 cursor-pointer'>Credit Score</span>
+                    
+                    <span className='flex flex-row gap-2 hover:text-[#F16122] transform duration-300 cursor-pointer'>Business <GrDown className='pt-1 text-xl'/></span>
+                    
+                    <span className='hover:text-[#F16122] transform duration-300 cursor-pointer'>About</span>
+                    
+                    <span className='hover:text-[#F16122] transform duration-300 cursor-pointer'>Career</span>
+                </div>
+
+                <button className='bg-[#F16122] rounded-3xl pt-2 pb-2 pl-6 pr-6 font-bold cursor-pointer'>Login</button>
+            </div>
+        </>
+    )
+}
+
+export default Header
