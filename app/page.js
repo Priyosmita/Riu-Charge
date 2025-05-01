95e859758522ed8f5b7195cc1e585d7edf0b7a0c
Priyosmita <daspriyosmita2003@gmail.com>
2025-05-01 18:40:18 +0530

    section

--- app/page.js
+++ app/page.js
@@ -4,14 +4,17 @@ import Header from './components/Header'
 const page = () => {
   return (
     <>
-    <Header/>
+      <Header />
 
-    {/* first section */}
-    <div className='bg-[#EBF2FB] h-44 w-full flex justify-center'>
-      <div className='bg-[#ffffff] h-40 w-7xl rounded-4xl'>
-
+      {/* first section */}
+      <div className='bg-[#EBF2FB] h-auto w-full flex justify-center'>
+        <div className='bg-[#ffffff] h-40 w-7xl rounded-4xl mt-9 mb-9'>
+          <div className='flex flex-row justify-center space-x-44'>
+            <div className='text-black'>fast</div>
+            <div>animation</div>
+          </div>
+        </div>
       </div>
-    </div>
     </>
   )
 }
