02073093c35cfd099607fdba64f259f4cd989e9e
Priyosmita <daspriyosmita2003@gmail.com>
2025-05-08 02:06:34 +0530

    footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,25 +1,44 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
-  return (
-    <>
-    <div className='w-full h-auto bg-[#444547]'>
-        <div className='flex flex-row justify-between pt-10 pl-36 pr-36 pb-10'>
-            <div className='text-amber-300'>
-                Company name
-            </div>
+    return (
+        <>
+            <div className='w-full h-auto bg-[#2e3136]'>
+                <div className='flex flex-row justify-between pt-10 pl-36 pr-36 pb-10'>
+                    <div className='flex flex-col space-y-9'>
+                        <div className='bg-[#d10f0f]'>
+                            Riucharge logo
+                        </div>
+                        <Link href="">
+                            <div>
+                                About Us
+                            </div>
+                        </Link>
+                        <Link href="">
+                            <div>
+                                Privacy Policy
+                            </div>
+                        </Link>
+                        <Link href="">
+                            <div>
+                                Terms and Conditions
+                            </div>
+                        </Link>
 
-            <div className='text-amber-300'>
-                social
-            </div>
+                    </div>
 
-            <div className='text-amber-300'>
-                services
+                    <div className='text-amber-300'>
+                        services
+                    </div>
+
+                    <div className='text-amber-300'>
+                        social
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Footer
