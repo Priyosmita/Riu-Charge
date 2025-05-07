e71944f5fff02ca7faff171d13ef2ed036c0d3b4
Priyosmita <daspriyosmita2003@gmail.com>
2025-05-07 23:33:43 +0530

    home

--- app/page.js
+++ app/page.js
@@ -8,12 +8,11 @@ const page = () => {
 
       {/* first section */}
       <div className='bg-[#EBF2FB] h-auto w-full flex justify-center'>
-        <div className='bg-[#ffffff] h-40 w-7xl rounded-4xl mt-9 mb-9'>
-          <div className='flex flex-row justify-center space-x-44'>
-            <div className='text-black'>fast</div>
-            <div>animation</div>
-          </div>
-        </div>
+      <img
+          src="/home pic.jpg"
+          alt="d2h logo"
+          className="h-auto w-full"
+        />
       </div>
 
       {/* second section */}
