5eb4619b3cdbdf3a4157eec03262c1f2c31f3210
Priyosmita <daspriyosmita2003@gmail.com>
2025-05-09 00:08:09 +0530

    section

--- app/components/FirstSectionHome.js
+++ app/components/FirstSectionHome.js
@@ -3,7 +3,7 @@ import React from 'react'
 const FirstSectionHome = () => {
   return (
     <>
-      <div className='flex justify-center relative'>
+      <div className='flex relative'>
         <img
           src="/home pic.jpg"
           alt="smiling woman"
@@ -13,15 +13,15 @@ const FirstSectionHome = () => {
         {/* Overlay */}
         <div className="absolute top-0 left-0 w-full h-full bg-[#000000] opacity-40 z-0"></div>
 
-        <div className="absolute z-10 flex w-full justify-center">
-  <div className='grid grid-cols-2 gap-6'>
-    {/* 4 equal-sized blocks */}
-    <div className='p-4 border-2 border-[#d10f0f] text-center w-40'>mobile recharge</div>
-    <div className='p-4 border-2 border-[#d10f0f] text-center w-40'>dth recharge</div>
-    <div className='p-4 border-2 border-[#d10f0f] text-center w-40'>big payments</div>
-    <div className='p-4 border-2 border-[#d10f0f] text-center w-40'>big sale deals</div>
-  </div>
-</div>
+        <div className="absolute z-10 flex w-full justify-end p-44">
+          <div className='grid grid-cols-2 gap-6'>
+            {/* 4 equal-sized blocks */}
+            <div className='p-4 border-2 border-[#d10f0f] text-center w-40 rounded-2xl'>mobile recharge</div>
+            <div className='p-4 border-2 border-[#d10f0f] text-center w-40'>dth recharge</div>
+            <div className='p-4 border-2 border-[#d10f0f] text-center w-40'>big payments</div>
+            <div className='p-4 border-2 border-[#d10f0f] text-center w-40'>big sale deals</div>
+          </div>
+        </div>
 
       </div>
     </>
