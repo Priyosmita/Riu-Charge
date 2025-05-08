3475349885c1d83ad75208b4a3b37bdaf6355127
Priyosmita <daspriyosmita2003@gmail.com>
2025-05-08 14:42:06 +0530

    footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -6,81 +6,85 @@ const Footer = () => {
         <>
             <div className='w-full h-auto bg-[#2e3136]'>
                 <div className='flex flex-row justify-between pt-16 pl-36 pr-36 pb-16'>
-                    
+
                     {/* col 1 */}
                     <div className='flex flex-col space-y-9'>
                         <span>
-                        <div className='bg-[#d10f0f] inline-block'>
-                            Riucharge logo
-                        </div>
+                            <div className='bg-[#d10f0f] inline-block'>
+                                Riucharge logo
+                            </div>
                         </span>
 
                         <span>
-                        <Link href="" className='inline-block'>
-                            <div className='hover:text-[#d10f0f] transform duration-300'>
-                                About Us
-                            </div>
-                        </Link>
-                        </span>
-                        
-                        <span>
-                        <Link href="" className='inline-block'>
-                            <div className='hover:text-[#d10f0f] transform duration-300'>
-                                Contact Us
-                            </div>
-                        </Link>
-                        </span>
-                        
-                        <span>
-                        <Link href="" className='inline-block'>
-                            <div className='hover:text-[#d10f0f] transform duration-300'>
-                                Privacy Policy
-                            </div>
-                        </Link>
+                            <Link href="" className='inline-block'>
+                                <div className='hover:text-[#d10f0f] transform duration-300'>
+                                    About Us
+                                </div>
+                            </Link>
                         </span>
 
                         <span>
-                        <Link href="" className='inline-block'>
-                            <div className='hover:text-[#d10f0f] transform duration-300'>
-                            Terms and Conditions
-                            </div>
-                        </Link>
+                            <Link href="" className='inline-block'>
+                                <div className='hover:text-[#d10f0f] transform duration-300'>
+                                    Contact Us
+                                </div>
+                            </Link>
+                        </span>
+
+                        <span>
+                            <Link href="" className='inline-block'>
+                                <div className='hover:text-[#d10f0f] transform duration-300'>
+                                    Privacy Policy
+                                </div>
+                            </Link>
+                        </span>
+
+                        <span>
+                            <Link href="" className='inline-block'>
+                                <div className='hover:text-[#d10f0f] transform duration-300'>
+                                    Terms and Conditions
+                                </div>
+                            </Link>
                         </span>
 
                         <span className="inline-block">
-  <Link href="/" className="hover:text-[#d10f0f] transition duration-300">
-    dg
-  </Link>
-</span>
+                            <Link href="/" className="hover:text-[#d10f0f] transition duration-300">
+                                dg
+                            </Link>
+                        </span>
                     </div>
 
 
                     {/*  col 2 */}
                     <div className='flex flex-col space-y-9'>
-                        <div>
-                            Our Services
-                        </div>
-                        <Link href="">
-                            <div className='inline-block'>
+                        <span className='text-xl font-semiboldbold'>
+                            OUR SERVICES
+                        </span>
+                        <span className="inline-block">
+                            <Link href="/" className="hover:text-[#d10f0f] transition duration-300">
                             Mobile Recharge
-                            </div>
-                        </Link>
-                        <Link href="">
-                            <div className='inline-block'>
+                            </Link>
+                        </span>
+                        
+                        <span className="inline-block">
+                            <Link href="/" className="hover:text-[#d10f0f] transition duration-300">
                             DTH Recharge
-                            </div>
-                        </Link>
-                        <Link href="">
-                            <div className='inline-block'>
+                            </Link>
+                        </span>
+                        
+                        <span className="inline-block">
+                            <Link href="/" className="hover:text-[#d10f0f] transition duration-300">
                             Bill Payments
-                            </div>
-                        </Link>
-                        <Link href="">
-                            <div className='inline-block'>
+                            </Link>
+                        </span>
+                        
+                        <span className="inline-block">
+                            <Link href="/" className="hover:text-[#d10f0f] transition duration-300">
                             Big Sale Deals
-                            </div>
-                        </Link>
+                            </Link>
+                        </span>
                     </div>
+
 
                     {/* col 3 */}
                     <div className='text-amber-300'>
