6c371d4056668421a2e6c49cc038df5dcab308e2
Priyosmita <daspriyosmita2003@gmail.com>
2025-05-08 12:16:36 +0530

    footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,33 +5,75 @@ const Footer = () => {
     return (
         <>
             <div className='w-full h-auto bg-[#2e3136]'>
-                <div className='flex flex-row justify-between pt-10 pl-36 pr-36 pb-10'>
+                <div className='flex flex-row justify-between pt-16 pl-36 pr-36 pb-16'>
                     
                     {/* col 1 */}
                     <div className='flex flex-col space-y-9'>
+                        <span>
                         <div className='bg-[#d10f0f] inline-block'>
                             Riucharge logo
                         </div>
-                        <Link href="">
-                            <div className='inline-block'>
+                        </span>
+
+                        <span>
+                        <Link href="" className='inline-block'>
+                            <div className=''>
                                 About Us
                             </div>
                         </Link>
-                        <Link href="">
-                            <div className='inline-block'>
+                        </span>
+                        
+                        <span>
+                        <Link href="" className='inline-block'>
+                            <div className=''>
+                                Contact Us
+                            </div>
+                        </Link>
+                        </span>
+                        
+                        <span>
+                        <Link href="" className='inline-block'>
+                            <div className=''>
                                 Privacy Policy
                             </div>
                         </Link>
-                        <Link href="">
-                            <div className='inline-block'>
-                                Terms and Conditions
+                        </span>
+
+                        <span>
+                        <Link href="" className='inline-block'>
+                            <div className=''>
+                            Terms and Conditions
                             </div>
                         </Link>
+                        </span>
                     </div>
 
+
                     {/*  col 2 */}
-                    <div className='text-amber-300'>
-                        services
+                    <div className='flex flex-col space-y-9'>
+                        <div>
+                            Our Services
+                        </div>
+                        <Link href="">
+                            <div className='inline-block'>
+                            Mobile Recharge
+                            </div>
+                        </Link>
+                        <Link href="">
+                            <div className='inline-block'>
+                            DTH Recharge
+                            </div>
+                        </Link>
+                        <Link href="">
+                            <div className='inline-block'>
+                            Bill Payments
+                            </div>
+                        </Link>
+                        <Link href="">
+                            <div className='inline-block'>
+                            Big Sale Deals
+                            </div>
+                        </Link>
                     </div>
 
                     {/* col 3 */}
