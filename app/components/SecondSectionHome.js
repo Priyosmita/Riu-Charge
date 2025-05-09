7ab8ae0a5e279efce34f8f44a7b7dbf61437b4cb
Priyosmita <daspriyosmita2003@gmail.com>
2025-05-09 16:34:53 +0530

    section

--- app/components/SecondSectionHome.js
+++ app/components/SecondSectionHome.js
@@ -3,50 +3,54 @@ import React from 'react'
 const SecondSectionHome = () => {
   return (
     <>
-    <div className='flex flex-row justify-center items-center bg-[#EBF2FB] h-24 space-x-16'>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Reliance_Jio_Logo_%28October_2015%29.svg/768px-Reliance_Jio_Logo_%28October_2015%29.svg.png"
-          alt="jio logo"
-          className="h-14 w-auto"
-        />
+      <div className='bg-[#EBF2FB] flex flex-col'>
+        <p className='text-[#424141] pt-7 text-2xl flex justify-center'>Exclusive Offers and Discounts: Recharge and Save with Special Deals!</p>
+        <div className='flex flex-row justify-center items-center h-40 space-x-24'>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Reliance_Jio_Logo_%28October_2015%29.svg/768px-Reliance_Jio_Logo_%28October_2015%29.svg.png"
+            alt="jio logo"
+            className="h-20 w-auto"
+          />
 
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/f/f4/D2h_logo_Brand.png?20181112043612"
-          alt="d2h logo"
-          className="h-14 w-auto"
-        />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/f/f4/D2h_logo_Brand.png?20181112043612"
+            alt="d2h logo"
+            className="h-20 w-auto"
+          />
 
-        <img
-          src="https://m.media-amazon.com/images/I/31BDVWh-9ZL.png"
-          alt="tata play binge logo"
-          className="h-26 w-auto"
-        />
+          <img
+            src="https://m.media-amazon.com/images/I/31BDVWh-9ZL.png"
+            alt="tata play binge logo"
+            className="h-36 w-auto"
+          />
 
-        <img
-          src="https://cdn.freelogovectors.net/wp-content/uploads/2023/05/airtel-logo-01-freelogovectors.net_.png"
-          alt="airtel logo"
-          className="h-14 w-auto"
-        />
+          <img
+            src="https://cdn.freelogovectors.net/wp-content/uploads/2023/05/airtel-logo-01-freelogovectors.net_.png"
+            alt="airtel logo"
+            className="h-20 w-auto"
+          />
 
-        <img
-          src="https://images.seeklogo.com/logo-png/43/2/vodafone-idea-logo-png_seeklogo-431500.png"
-          alt="vi logo"
-          className="h-14 w-auto"
-        />
+          <img
+            src="https://images.seeklogo.com/logo-png/43/2/vodafone-idea-logo-png_seeklogo-431500.png"
+            alt="vi logo"
+            className="h-24 w-auto"
+          />
 
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Tata_Power_Logo.svg/1200px-Tata_Power_Logo.svg.png"
-          alt="tata power logo"
-          className="h-6 w-auto"
-        />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Tata_Power_Logo.svg/1200px-Tata_Power_Logo.svg.png"
+            alt="tata power logo"
+            className="h-9 w-auto"
+          />
 
-        <img
-          src="https://www.pinpng.com/pngs/m/613-6130497_sun-direct-dth-247-customer-care-toll-free.png"
-          alt="sun direct logo"
-          className="h-12 w-auto"
-        />
+          <img
+            src="https://www.pinpng.com/pngs/m/613-6130497_sun-direct-dth-247-customer-care-toll-free.png"
+            alt="sun direct logo"
+            className="h-16 w-auto"
+          />
 
+        </div>
       </div>
+
     </>
   )
 }
