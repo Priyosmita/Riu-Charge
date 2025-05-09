ce8b15b4a82b3317bf6d83cd6c38a2ed9fa16651
Priyosmita <daspriyosmita2003@gmail.com>
2025-05-09 15:51:58 +0530

    section

--- app/components/FirstSectionHome.js
+++ app/components/FirstSectionHome.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaMobileAlt,FaFileInvoiceDollar } from "react-icons/fa";
+import { FaMobileAlt,FaFileInvoiceDollar,FaTags } from "react-icons/fa";
 import { FaTv } from "react-icons/fa6";
 
 const FirstSectionHome = () => {
@@ -18,14 +18,14 @@ const FirstSectionHome = () => {
         <div className="absolute z-10 flex w-full justify-end p-44">
           <div className='grid grid-cols-2 gap-6'>
             {/* 4 equal-sized blocks */}
-            <div className='p-4 border-2 border-[#d10f0f] text-center w-40 rounded-2xl flex flex-col'>
-              <FaMobileAlt />
+            <div className='p-6 border-2 border-[#d1cfcf] text-center w-52 h-56 rounded-2xl flex flex-col items-center bg-[#ffffff3d]'>
+              <FaMobileAlt className='text-[#d10f0f] text-4xl' />
               <p>Mobile Recharge</p>
               <p>Instant mobile recharges with top providers</p>
               <button>Recharge Now</button>
             </div>
 
-            <div className='p-4 border-2 border-[#d10f0f] text-center w-40 rounded-2xl flex flex-col'>
+            <div className='p-4 border-2 border-[#d10f0f] text-center w-60 h-64 rounded-2xl flex flex-col'>
               <FaTv />
               <p>DTH Recharge</p>
               <p>Top-up your favorite DTH services hassle-free</p>
@@ -40,7 +40,7 @@ const FirstSectionHome = () => {
             </div>
 
             <div className='p-4 border-2 border-[#d10f0f] text-center w-40 rounded-2xl flex flex-col'>
-              <FaTv />
+              <FaTags />
               <p>Big Sale Deals</p>
               <p>Grab amazing discounts and cashback offers</p>
               <button>Shop Now</button>
